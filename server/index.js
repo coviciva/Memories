aa0b06f892c20a1d93b6da59933d296b449c099e
coviciva <icovic01@fesb.hr>
2021-04-01 16:30:09 +0200

    create post

--- server/index.js
+++ server/index.js
@@ -7,11 +7,11 @@ const postRoutes = require("./routes/posts");
 
 const app = express();
 
-app.use("/posts", postRoutes);
-
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+
+app.use("/posts", postRoutes);
 
 const CONNECTION_URL =
   "mongodb+srv://javascriptmastery:javascriptmastery123@cluster0.z2svp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
